0e640ecb751f45c33f47d4958350bd3cbabcc686
Pete Davis <7900180+petedavisdev@users.noreply.github.com>
2020-05-18 18:54:47 +0100

    Create Post “interactive-content-with-components-in-markdown-on-vuepress” (#54)
    * Create Post “interactive-content-with-components-in-markdown-on-vuepress”
    
    * Update Post “interactive-content-with-components-in-markdown-on-vuepress”
    
    * Update Post “interactive-content-with-components-in-markdown-on-vuepress”
    
    * Update Post “interactive-content-with-components-in-markdown-on-vuepress”
    
    * Update Post “interactive-content-with-components-in-markdown-on-vuepress”
    
    * Update Post “interactive-content-with-components-in-markdown-on-vuepress”
    
    * Update Post “interactive-content-with-components-in-markdown-on-vuepress”
    
    * Update Post “interactive-content-with-components-in-markdown-on-vuepress”
    
    * Update Post “interactive-content-with-components-in-markdown-on-vuepress”
    
    * Update Post “interactive-content-with-components-in-markdown-on-vuepress”
    
    * Update Post “interactive-content-with-components-in-markdown-on-vuepress”
    
    * Update Post “interactive-content-with-components-in-markdown-on-vuepress”
    
    * ProfileCard.vue
    
    * True or false
    
    * Update Post “interactive-content-with-components-in-markdown-on-vuepress”
    
    * Update Post “interactive-content-with-components-in-markdown-on-vuepress”
    
    * Update Post “interactive-content-with-components-in-markdown-on-vuepress”
    
    * Update Post “interactive-content-with-components-in-markdown-on-vuepress”
    
    * Update Post “interactive-content-with-components-in-markdown-on-vuepress”
    
    * Update Post “interactive-content-with-components-in-markdown-on-vuepress”
    
    * Update Post “interactive-content-with-components-in-markdown-on-vuepress”
    
    * rename file
    
    * add tag
    
    * date and .md correction
    
    * Finish post
    
    * content edit

---
+++ .vuepress/components/ProfileCard.vue
@@ -0,0 +1,27 @@
+<template>
+<section class="profile-card">
+    <img class="profile-img" :src="$attrs.imgSrc" />
+    <div>
+        <slot /> <!-- This is where your content will be inserted -->
+    </div>
+</section>
+</template>
+
+<style scoped>
+.profile-card {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    grid-gap: 1em;
+    max-width: 350px;
+    margin: 2em auto;
+    border-radius: 0.5em;
+    background-color: #fff1;
+    box-shadow: 0.1em 0.1em 0.5em 0.5em #0003;
+}
+
+.profile-img {
+    box-sizing: border-box;
+    padding: 1em;
+    border-radius: 50%;
+}
+</style>
